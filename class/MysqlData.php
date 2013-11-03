9d39833262d6cd5cd303d458fd28c16688239386
eamania <eamania02@gmail.com>
2013-11-03 14:19:44 +0100

    MySqlData Prova

--- class/MysqlData.php
+++ class/MysqlData.php
@@ -4,7 +4,7 @@ class MysqlData extends myPDO {
 
     CONST DATABASE_NAME = "AcquarioGest";
     CONST DATABASE_USERNAME = "root";
-    CONST DATABASE_PASSWORD = "root";
+    CONST DATABASE_PASSWORD = "";
     CONST DATABASE_SERVER = "localhost";
     CONST DATABASE_DNS = "";
  
