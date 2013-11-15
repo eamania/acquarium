3e5fedca919adf6ba2e504f553cd4caeb74093f1
eamania <eamania@192.168.1.5>
2013-11-15 21:51:27 +0100

    Bò

--- header.php
+++ header.php
@@ -14,8 +14,40 @@ $db = new myPDO("mysql:host=$dbDate->DatabaseServer;dbname=$dbDate->DatabaseName
 
     </head>
     <body>
-<nav class="navbar navbar-default " role="navigation">
-    <span class="label label-danger">Acquarium Simple
-</span>
-    <button type="button" class="btn btn-default navbar-btn right">Sign in</button>
+        
+        <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+      <a class="navbar-brand" href="index.php">MyAcquarium</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Acquarium Home Page</a></li>
+      <li><a href="acquarium.php">Acquarium</a></li>
+      <li class="dropdown">
+        <a href="fish.php" class="dropdown-toggle" data-toggle="dropdown">Fish <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+            <li><a href="fish.php">Add</a></li>
+        </ul>
+      </li>
+    </ul>
+      
+    <form class="navbar-form navbar-right" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Cerca</button>
+    </form>
+      
+      
+    
+  </div><!-- /.navbar-collapse -->
 </nav>
